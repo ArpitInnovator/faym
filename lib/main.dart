1b60b31785dd2bc94811d1ec11a943c877bcbd6e
Arpit <arpitprofessional04@gmail.com>
2026-02-03 02:51:15 +0530

    lttle fix

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Faym',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
